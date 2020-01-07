16c998bd37878ec250987beeb9f6c6a8482c62d8
Lee Kah Seng <yeos_lee@yahoo.com.sg>
2020-01-07 14:58:25 +0800

    Initial commit

---
+++ SwiftSenpai-FB-Integration-Demo/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SwiftSenpai-FB-Integration-Demo
+//
+//  Created by Lee Kah Seng on 07/01/2020.
+//  Copyright © 2020 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let loginButton = FBLoginButton()
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        
+        NotificationCenter.default.addObserver(forName: .AccessTokenDidChange, object: nil, queue: OperationQueue.main) { (notification) in
+            
+            print("FB Access Token: \(String(describing: AccessToken.current?.tokenString))")
+        }
+        
+//        FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+//         // Optional: Place the button in the center of your view.
+//         loginButton.center = self.view.center;
+//         [self.view addSubview:loginButton];
+    }
+
+
+}
+
