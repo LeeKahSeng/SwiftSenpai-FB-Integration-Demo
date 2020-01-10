d46c4d69e3e9e0564c965191799ff94c781c631c
Lee Kah Seng <yeos_lee@yahoo.com.sg>
2020-01-10 10:17:00 +0800

    Added comment

--- SwiftSenpai-FB-Integration-Demo/ViewController.swift
+++ SwiftSenpai-FB-Integration-Demo/ViewController.swift
@@ -9,22 +9,23 @@
 import UIKit
 import FBSDKLoginKit
 
-
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Add FBLoginButton at center of view controller
         let loginButton = FBLoginButton()
         loginButton.center = view.center
         view.addSubview(loginButton)
         
+        // Observe access token changes
+        // This will trigger after successfully login / logout
         NotificationCenter.default.addObserver(forName: .AccessTokenDidChange, object: nil, queue: OperationQueue.main) { (notification) in
             
+            // Print out access token
             print("FB Access Token: \(String(describing: AccessToken.current?.tokenString))")
         }
     }
-
-
 }
 
