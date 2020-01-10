a729b8963b49c3ac12dd8b7dc6e7c55126f9a663
Lee Kah Seng <yeos_lee@yahoo.com.sg>
2020-01-10 10:49:23 +0800

    Setup view controller for custom login button

---
+++ SwiftSenpai-FB-Integration-Demo/CustomButtonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomButtonViewController.swift
+//  SwiftSenpai-FB-Integration-Demo
+//
+//  Created by Lee Kah Seng on 10/01/2020.
+//  Copyright © 2020 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+
+class CustomButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
