5228fc90324e7123f6ebf4c3e0a3471579df1ec0
Lee Kah Seng <yeos_lee@yahoo.com.sg>
2020-01-10 22:57:12 +0800

    Implemented reusable facebook login button

---
+++ SwiftSenpai-FB-Integration-Demo/Reusable FB Button/ReusableButtonViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ReusableButtonViewController.swift
+//  SwiftSenpai-FB-Integration-Demo
+//
+//  Created by Lee Kah Seng on 10/01/2020.
+//  Copyright © 2020 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ReusableButtonViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: MYFacebookLoginButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateMessage(with: Profile.current?.name)
+        
+        // Implement login completion handler
+        loginButton.loginCompletionHandler = { [weak self] (button, result) in
+            switch result {
+            case .success( _):
+                // Show message after login completed
+                Profile.loadCurrentProfile { (profile, error) in
+                    self?.updateMessage(with: Profile.current?.name)
+                }
+            case .failure( _):
+                break
+            }
+        }
+        
+        // Implement logout completion handler
+        loginButton.logoutCompletionHandler = { [weak self] (button) in
+            // Show message after logout completed
+            self?.updateMessage(with: nil)
+        }
+    }
+}
+
+// MARK:- Private functions
+extension ReusableButtonViewController {
+    
+    private func updateMessage(with name: String?) {
+        
+        guard let name = name else {
+            // User already logged out
+            messageLabel.text = "Please log in with Facebook."
+            return
+        }
+        
+        // User already logged in
+        messageLabel.text = "Hello, \(name)!"
+    }
+}
